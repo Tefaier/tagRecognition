7f129cf51d589d2519c30833ac58c73bad76f7ae
Tefaier <timabelov00@gmail.com>
2024-10-30 14:28:11 +0300

    testing script for apriltag

--- apriltag/test.py
+++ apriltag/test.py
@@ -7,20 +7,20 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 imagePath1 = "./testImages/img.png"
-imagePath2 = "./testImages/tag41_12_00014.png"
+imagePath2 = "./testImages/tag41_12_00000.png"
 
 # used
 # https://pyimagesearch.com/2020/11/02/apriltag-with-python/
 # https://matplotlib.org/stable/gallery/mplot3d/imshow3d.html#sphx-glr-gallery-mplot3d-imshow3d-py
 
 def imshow3d(ax, img):
-    xx, yy = np.meshgrid(np.linspace(0, img.shape[1] / img.shape[0], img.shape[1]), np.linspace(0, 1, img.shape[0]))
-    zz     = np.ones((img.shape[0],img.shape[1]))
+    xx, yy = np.meshgrid(np.linspace(0, img.shape[1] / img.shape[0], img.shape[1] + 1), np.linspace(0, 1, img.shape[0] + 1))
+    zz     = np.ones((img.shape[0] + 1,img.shape[1] + 1))
     ax.plot_surface(xx, yy, zz, rstride=1, cstride=1, facecolors=img, shade=False)
 
 def createDetector() -> Detector:
     return Detector(searchpath=['apriltags'],
-                       families='tag36h11', # tagStandard41h12 tag25h9 tag36h11
+                       families='tagStandard41h12', # tagStandard41h12 tag25h9 tag36h11
                        nthreads=1,
                        quad_decimate=1.0,
                        quad_sigma=0.0,
@@ -36,13 +36,17 @@ def createRotatedImage(path: str, reduce: int = 1) -> str:
     if (image.shape[-1] == 4):
         image = image[:, :, :-1]
 
+    plt.axis('square')
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ax.view_init(elev=90, azim=0, roll=0)
+    ax.view_init(elev=-90, azim=0, roll=0)
     ax.grid(False)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_zticks([])
+    # expect to width be >= height ALWAYS
+    plt.xlim(0, image.shape[1] / image.shape[0])
+    plt.ylim(0, image.shape[1] / image.shape[0])
     imshow3d(ax, img=image / 255)
     newName = '.'.join(path.split('.')[:-1]) + "3d.png"
     plt.savefig(newName, dpi=200, bbox_inches='tight', pad_inches=0)
@@ -78,7 +82,7 @@ def analyseImage(results: tuple[list[Detection], Any] | list[Detection], image: 
 
 
 detector = createDetector()
-rotatedVersion = imagePath1 if True else createRotatedImage(imagePath2, reduce=1)
+rotatedVersion = imagePath1 if True else createRotatedImage(imagePath1, reduce=4)
 image = getGrayImage(rotatedVersion)
 results = detector.detect(image)
 print(results)
