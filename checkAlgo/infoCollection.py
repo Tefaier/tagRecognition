ba4200c8039588838d6da4c6a70714c31dbae181
Tefaier <timabelov00@gmail.com>
2024-11-06 19:41:00 +0300

    undistortion before writing image

--- checkAlgo/infoCollection.py
+++ checkAlgo/infoCollection.py
@@ -2,8 +2,15 @@ import os
 
 import cv2
 import pandas as pd
+import numpy as np
 
 from checkAlgo.constantsForCheck import collectionFolder, csvName
+
+w = 100
+h = 100
+camMatrix = np.array([[804.7329058535828, 0.0, 549.3237487667773], [0.0, 802.189566021595, 293.62680986426403], [0.0, 0.0, 1.0]])
+distCoeffs = np.array([-0.12367717208987415, 1.3006314330799533, -0.00045665885332229637, -0.028794247586331707, -2.264152794148503])
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camMatrix, distCoeffs, (w,h), 0, (w,h))
 
 # fields: imageName, tagFamily, tagId, transform, rotation
 imageNames = []
@@ -18,6 +25,7 @@ toWriteFrom = max(files) + 1
 iterationIndex = 0
 for i in range(0, 0):
     image = cv2.imread("")
+    image = cv2.undistort(image, camMatrix, distCoeffs, None, newcameramtx)
     # write image
     cv2.imwrite(filename=(collectionFolder + "/" + str(toWriteFrom + iterationIndex) + ".png"), img=image)
     # fill values
