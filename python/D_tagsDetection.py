078f6b9f8dfa9996dba3fe4f8ddb9e67b859fb38
Timofei <timabelov08@gmail.com>
2025-01-26 12:25:00 +0300

    info preparation step merged into detection step

--- python/D_tagsDetection.py
+++ python/D_tagsDetection.py
@@ -11,30 +11,64 @@ from python.models.transformsParser.simpleParser import SimpleParser
 from python.models.transformsParser.transformsParser import TransformsParser
 from python.settings import generatedInfoFolder, imageInfoFilename, detectionInfoFilename, analyseImagesFolder, \
     generalInfoFilename
+from python.utils import parseRotation, readStringOfList
+
 
 def openAndPrepareRawInfo(path: str) -> pd.DataFrame:
     info = pd.read_csv(path)
     # info = info.reset_index()
     return info
 
+def getVectorError(vector1: list, vector2: list) -> list:
+    if (len(vector1) == 0 or len(vector2) == 0): return []
+    return [vector2[i] - vector1[i] for i in range(0, len(vector1))]
+
+
+def getRotationError(rotation1: list, rotation2: list) -> list:
+    rotation1 = parseRotation(rotation1)
+    rotation2 = parseRotation(rotation2)
+    if (rotation1 == None or rotation2 == None): return []
+
+    rotation1To2 = rotation2 * rotation1.inv()
+    return rotation1To2.as_rotvec(degrees=False).tolist()
+
 def analyseInfo(imagesFolder: str, detector: TagDetector, dframe: pd.DataFrame, parser: TransformsParser):
     detectorName = np.full((dframe.shape[0],), detector.name)
     detectedT = []
     detectedR = []
 
-    performDetection(imagesFolder, detector, dframe, detectedT, detectedR, parser)
+    writeDetectionInfo(imagesFolder, detector, dframe, detectedT, detectedR, parser)
     dframe["method"] = detectorName
     dframe["detectedT"] = detectedT
     dframe["detectedR"] = detectedR
+    writeErrorInfo(dframe)
 
-def performDetection(imagesFolder: str, detector: TagDetector, dframe: pd.DataFrame, translationWrite: list, rotationWrite: list, parser: TransformsParser):
+def writeErrorInfo(dframe: pd.DataFrame):
+    realT = readStringOfList(dframe['realT'])
+    realR = readStringOfList(dframe['realR'])
+    detectedT = readStringOfList(dframe['detectedT'])
+    detectedR = readStringOfList(dframe['detectedR'])
+
+    errorT = []
+    errorR = []
+    isSuccess = np.full((dframe.shape[0],), False)
+    for i in range(0, dframe.shape[0]):
+        errorT.append(getVectorError(realT[i], detectedT[i]))
+        errorR.append(getRotationError(realR[i], detectedR[i]))
+        isSuccess[i] = len(errorT[-1]) != 0 and len(errorR[-1]) != 0
+
+    dframe['isSuccess'] = isSuccess
+    dframe['errorT'] = errorT
+    dframe['errorR'] = errorR
+
+def writeDetectionInfo(imagesFolder: str, detector: TagDetector, dframe: pd.DataFrame, translationWrite: list, rotationWrite: list, parser: TransformsParser):
     for _, row in dframe.iterrows():
         t, r, ids = detector.detect(image=cv2.imread(f"{imagesFolder}/{row["imageName"]}"))
         t, r = parser.getParentTransform(t, r, ids)
         translationWrite.append([float(val) for val in t])
         rotationWrite.append([float(val) for val in r])
 
-def writeInfo(path: str, dframe: pd.DataFrame, detectionSettings: dict, replace: bool):
+def writeInfoToFile(path: str, dframe: pd.DataFrame, detectionSettings: dict, replace: bool):
     dframe["detectionSettings"] = np.full((dframe.shape[0],), detectionSettings)
 
     if replace or not os.path.exists(path):
@@ -47,7 +81,7 @@ def performDetection(profile: str, detector: TagDetector, detectionSettings: dic
     profilePath = f"{os.path.dirname(__file__)}/{generatedInfoFolder}/{profile}"
     imagesInfo = openAndPrepareRawInfo(f"{profilePath}/{imageInfoFilename}.csv")
     analyseInfo(f"{profilePath}/{analyseImagesFolder}", detector, imagesInfo, parser)
-    writeInfo(
+    writeInfoToFile(
         f"{profilePath}/{detectionInfoFilename}.csv",
         imagesInfo,
         detectionSettings,
