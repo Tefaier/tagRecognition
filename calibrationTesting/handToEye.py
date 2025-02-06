1491b3c947e827c293c6a3ae4e5469736d23e40c
Timofei <timabelov08@gmail.com>
2025-02-06 19:39:29 +0300

    old constants file moved

--- calibrationTesting/handToEye.py
+++ calibrationTesting/handToEye.py
@@ -10,7 +10,7 @@ from vtkmodules.vtkFiltersSources import vtkPlaneSource
 from vtkmodules.vtkIOImage import vtkImageReader2Factory, vtkPNGWriter
 from vtkmodules.vtkRenderingCore import vtkTexture, vtkPolyDataMapper, vtkActor, vtkRenderer, vtkRenderWindow, \
     vtkWindowToImageFilter
-from python.constantsForCheck import imageWidth, imageHeight, camMatrix, distortionCoefficients
+from constantsForCheck import imageWidth, imageHeight, camMatrix, distortionCoefficients
 
 cameraRotation = Rotation.from_rotvec([0, 0, 0], degrees=True)
 cameraTranslation = np.array([0, 0, 0])
